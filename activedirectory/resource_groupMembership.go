61dd7dfc98f24b962dea393c1b60f8b8b1e6f3f0
Ray Terrill <rayterrill@users.noreply.github.com>
2018-11-14 08:30:44 -0800

    Update resource_groupMembership.go

--- activedirectory/resource_groupMembership.go
+++ activedirectory/resource_groupMembership.go
@@ -1,10 +1,10 @@
 package activedirectory
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/portofportland/goWinRM"
+        "github.com/hashicorp/terraform/helper/schema"
+        "github.com/portofportland/goPSRemoting"
 
-	"strings"
+        "strings"
 )
 
 func resourcegroupMembership() *schema.Resource {
@@ -34,70 +34,70 @@ func resourcegroupMembership() *schema.Resource {
 }
 
 func resourcegroupMembershipCreate(d *schema.ResourceData, m interface{}) error {
-	//convert the interface so we can use the variables like username, etc
-	client := m.(*ActiveDirectoryClient)
+        //convert the interface so we can use the variables like username, etc
+        client := m.(*ActiveDirectoryClient)
 
-	object_name := d.Get("object_name").(string)
-	object_class := d.Get("object_class").(string)
-	group_name := d.Get("group_name").(string)
+        object_name := d.Get("object_name").(string)
+        object_class := d.Get("object_class").(string)
+        group_name := d.Get("group_name").(string)
 
-	var id string = object_name + "_" + object_class + "_" + group_name
+        var id string = object_name + "_" + object_class + "_" + group_name
 
-	var psCommand string = "$object = Get-ADObject -Filter {(name -eq '" + object_name + "') -AND (ObjectClass -eq '" + object_class + "')}; Add-ADGroupMember -Identity '" + group_name + "' -Members $object.DistinguishedName -Confirm:$false"
-	_, err := goWinRM.RunWinRMCommand(client.username, client.password, client.server, psCommand, client.usessl, client.usessh)
-	if err != nil {
-		//something bad happened
-		return err
-	}
+        var psCommand string = "$object = Get-ADObject -Filter {(name -eq \\\"" + object_name + "\\\") -AND (ObjectClass -eq \\\"" + object_class + "\\\")}; Add-ADGroupMember -Identity \\\"" + group_name + "\\\" -Members $object.DistinguishedName -Confirm:$false"
+        _, err := goPSRemoting.RunPowershellCommand(client.username, client.password, client.server, psCommand, client.usessl, client.usessh)
+        if err != nil {
+                //something bad happened
+                return err
+        }
 
-	d.SetId(id)
+        d.SetId(id)
 
-	return nil
+        return nil
 }
 
 func resourcegroupMembershipRead(d *schema.ResourceData, m interface{}) error {
-	//convert the interface so we can use the variables like username, etc
-	client := m.(*ActiveDirectoryClient)
+        //convert the interface so we can use the variables like username, etc
+        client := m.(*ActiveDirectoryClient)
 
-	object_name := d.Get("object_name").(string)
-	object_class := d.Get("object_class").(string)
-	group_name := d.Get("group_name").(string)
+        object_name := d.Get("object_name").(string)
+        object_class := d.Get("object_class").(string)
+        group_name := d.Get("group_name").(string)
 
-        var psCommand string = "$object = Get-ADGroupMember -Identity '" + group_name + "' | Where-Object {$_.Name -eq '" + object_name + "' -AND $_.objectClass -eq '" + object_class + "'}; if (!$object) { Write-Host 'TERRAFORM_NOT_FOUND' }"
-	stdout, err := goWinRM.RunWinRMCommand(client.username, client.password, client.server, psCommand, client.usessl, client.usessh)
-	if err != nil {
-		//something bad happened
-		return err
-	}
+        var psCommand string = "$object = Get-ADGroupMember -Identity \\\"" + group_name + "\\\" | Where-Object {$_.Name -eq \\\"" + object_name + "\\\" -AND $_.objectClass -eq \\\"" + object_class + "\\\"}; if (!$object) { Write-Host 'TERRAFORM_NOT_FOUND' }"
+        stdout, err := goPSRemoting.RunPowershellCommand(client.username, client.password, client.server, psCommand, client.usessl, client.usessh)
+        if err != nil {
+                //something bad happened
+                return err
+        }
 
-	if strings.Contains(stdout, "TERRAFORM_NOT_FOUND") {
-		//not able to find the record - this is an error but ok
-		d.SetId("")
-		return nil
-	}
+        if strings.Contains(stdout, "TERRAFORM_NOT_FOUND") {
+                //not able to find the record - this is an error but ok
+                d.SetId("")
+                return nil
+        }
 
-	var id string = object_name + "_" + object_class + "_" + group_name
-	d.SetId(id)
-	return nil
+        var id string = object_name + "_" + object_class + "_" + group_name
+        d.SetId(id)
+        return nil
 }
 
 func resourcegroupMembershipDelete(d *schema.ResourceData, m interface{}) error {
-	//convert the interface so we can use the variables like username, etc
-	client := m.(*ActiveDirectoryClient)
+        //convert the interface so we can use the variables like username, etc
+        client := m.(*ActiveDirectoryClient)
 
-	object_name := d.Get("object_name").(string)
-	object_class := d.Get("object_class").(string)
-	group_name := d.Get("group_name").(string)
+        object_name := d.Get("object_name").(string)
+        object_class := d.Get("object_class").(string)
+        group_name := d.Get("group_name").(string)
 
-	var psCommand string = "$object = Get-ADObject -Filter {(name -eq '" + object_name + "') -AND (ObjectClass -eq '" + object_class + "')}; Remove-ADGroupMember -Identity '" + group_name + "' -Members $object.DistinguishedName -Confirm:$false"
-	_, err := goWinRM.RunWinRMCommand(client.username, client.password, client.server, psCommand, client.usessl, client.usessh)
-	if err != nil {
-		//something bad happened
-		return err
-	}
+        var psCommand string = "$object = Get-ADObject -Filter {(name -eq \\\"" + object_name + "\\\") -AND (ObjectClass -eq \\\"" + object_class + "\\\")}; Remove-ADGroupMember -Identity \\\"" + group_name + "\\\" -Members $object.DistinguishedName -Confirm:$false"
+        _, err := goPSRemoting.RunPowershellCommand(client.username, client.password, client.server, psCommand, client.usessl, client.usessh)
+        if err != nil {
+                //something bad happened
+                return err
+        }
 
-	// d.SetId("") is automatically called assuming delete returns no errors, but it is added here for explicitness.
-	d.SetId("")
+        // d.SetId("") is automatically called assuming delete returns no errors, but it is added here for explicitness.
+        d.SetId("")
 
-	return nil
+        return nil
 }
