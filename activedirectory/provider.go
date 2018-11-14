03a27ac5e0ea92d3b6570b9af9b4384acbe522f9
Ray Terrill <rayterrill@users.noreply.github.com>
2018-11-14 07:58:26 -0800

    Update provider.go

--- activedirectory/provider.go
+++ activedirectory/provider.go
@@ -1,73 +1,74 @@
 package activedirectory
 
 import (
-	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/hashicorp/terraform/terraform"
+        "github.com/hashicorp/terraform/helper/schema"
+        "github.com/hashicorp/terraform/terraform"
 
-	"fmt"
+        "fmt"
 )
 
 // Provider allows making changes to Microsoft AD
 // Utilises Powershell to connect to domain controller
 func Provider() terraform.ResourceProvider {
-	return &schema.Provider{
-		Schema: map[string]*schema.Schema{
-			"username": &schema.Schema{
-				Type:        schema.TypeString,
-				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("USERNAME", nil),
-				Description: "Username to connect to AD.",
-			},
-			"password": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("PASSWORD", nil),
-				Description: "The password to connect to AD.",
-			},
-			"server": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("SERVER", nil),
-				Description: "The AD server to connect to.",
-			},
-			"usessl": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("USESSL", false),
-				Description: "Whether or not to use HTTPS to connect to WinRM",
-			},
-			"usessh": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("USESSH", false),
-				Description: "Whether or not to use SSH to connect to WinRM",
-			},
-			"default_computer_container": &schema.Schema{
-				Type:        schema.TypeString,
-				Optional:    true,
-				DefaultFunc: schema.EnvDefaultFunc("DEFAULT_COMPUTER_CONTAINER", nil),
-				Description: "The default computer container to move objects to on a delete - Defaults to '(Get-ADDomain | select computerscont*)'",
-			},
-		},
-		ResourcesMap: map[string]*schema.Resource{
-			"activedirectory_ouMapping": resourceOUMapping(),
-			"activedirectory_groupMembership": resourcegroupMembership(),
-		},
+        return &schema.Provider{
+                Schema: map[string]*schema.Schema{
+                        "username": &schema.Schema{
+                                Type:        schema.TypeString,
+                                Required:    true,
+                                DefaultFunc: schema.EnvDefaultFunc("USERNAME", nil),
+                                Description: "Username to connect to AD.",
+                        },
+                        "password": &schema.Schema{
+                                Type:        schema.TypeString,
+                                Optional:    true,
+                                DefaultFunc: schema.EnvDefaultFunc("PASSWORD", nil),
+                                Description: "The password to connect to AD.",
+                        },
+                        "server": &schema.Schema{
+                                Type:        schema.TypeString,
+                                Required:    true,
+                                DefaultFunc: schema.EnvDefaultFunc("SERVER", nil),
+                                Description: "The AD server to connect to.",
+                        },
+                        "usessl": &schema.Schema{
+                                Type:        schema.TypeString,
+                                Optional:    true,
+                                DefaultFunc: schema.EnvDefaultFunc("USESSL", false),
+                                Description: "Whether or not to use HTTPS to connect to WinRM",
+                        },
+                        "usessh": &schema.Schema{
+                                Type:        schema.TypeString,
+                                Optional:    true,
+                                DefaultFunc: schema.EnvDefaultFunc("USESSH", false),
+                                Description: "Whether or not to use SSH to connect to WinRM",
+                        },
+                        "default_computer_container": &schema.Schema{
+                                Type:        schema.TypeString,
+                                Optional:    true,
+                                DefaultFunc: schema.EnvDefaultFunc("DEFAULT_COMPUTER_CONTAINER", nil),
+                                Description: "The default computer container to move objects to on a delete - Defaults to '(Get-ADDomain | select computerscont*)'",
+                        },
+                },
+                ResourcesMap: map[string]*schema.Resource{
+                        "activedirectory_ouMapping": resourceOUMapping(),
+                        "activedirectory_groupMembership": resourcegroupMembership(),
+                },
 
-		ConfigureFunc: providerConfigure,
-	}
+                ConfigureFunc: providerConfigure,
+        }
 }
-
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	username := d.Get("username").(string)
 	if username == "" {
 		return nil, fmt.Errorf("The 'username' property was not specified.")
 	}
+	
+	usessh := d.Get("usessh").(string)
 
 	password := d.Get("password").(string)
-	if password == "" {
-		return nil, fmt.Errorf("The 'password' property was not specified.")
-	}
+        if password == "" && usessh == "0" {
+                return nil, fmt.Errorf("The 'password' property was not specified and usessh was false.")
+        }
 
 	server := d.Get("server").(string)
 	if server == "" {
@@ -75,7 +76,6 @@ func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	}
 
 	usessl := d.Get("usessl").(string)
-	usessh := d.Get("usessh").(string)
 	default_computer_container := d.Get("default_computer_container").(string)
 
 	client := ActiveDirectoryClient {
